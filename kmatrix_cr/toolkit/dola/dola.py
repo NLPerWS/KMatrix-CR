724c405a99ce680044a1dc0fab7f24e77e5160f7
wd <2272749111@qq.com>
2025-04-09 02:34:36 +0000

    update

--- kmatrix_cr/toolkit/dola/dola.py
+++ kmatrix_cr/toolkit/dola/dola.py
@@ -135,11 +135,11 @@ class DoLa:
                 #                         top_p=top_p, top_k=top_k, temperature=temperature, stopping_criteria=self.stopping_criteria, relative_top=relative_top, 
                 #                         mature_layer=mature_layer, premature_layer=None, candidate_premature_layers=candidate_premature_layers, **kwargs,)
                 
-                
+                # dola_layers=candidate_premature_layers,
                 outputs = self.model.generate(input_ids, max_length=max_len, num_return_sequences=1,
-                                        output_scores=True, return_dict_in_generate=True, dola_layers=candidate_premature_layers,
+                                        output_scores=True, return_dict_in_generate=True,
                                         top_p=top_p, top_k=top_k, temperature=temperature, stopping_criteria=self.stopping_criteria,
-                                         premature_layer=None,  **kwargs,)
+                                         premature_layer=premature_layer,  **kwargs)
                 
                 premature_layer_dist = None
                 
